9f602b8ccfd5ca6768192778f4bbd2072990cce3
Aaqib Samiullah <55548887+bajwaaaqib@users.noreply.github.com>
2026-08-12 11:39:56 +0400

    Update __manifest__.py

--- aaqify_backed_theme/__manifest__.py
+++ aaqify_backed_theme/__manifest__.py
@@ -29,5 +29,5 @@
     'application': True,
     'price': 0.00,
     'currency': 'USD',
-    'images': ['static/description/main_screenshot.png'],
+    'images': ['static/description/main_screenshot.gif'],
 }
